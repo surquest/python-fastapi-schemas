65a3604a20786716585dc6bfc2c911027e06c0b0
Michal Švarc <michal.svarc@surquest.com>
2023-02-27 18:06:35 +0100

    New management of responses

--- src/surquest/fastapi/schemas/responses/responses.py
+++ src/surquest/fastapi/schemas/responses/responses.py
@@ -1,39 +1,102 @@
-# from pydantic import BaseModel
 from .base import Base
 from .info import InfoSuccess, InfoWarning, InfoError
-from .metadata import Metadata
 from typing import Union, List, Dict, Optional
+from starlette.responses import JSONResponse
 
 
-class ResponseSuccess(Base):
+class Success(Base):
     info: InfoSuccess = InfoSuccess()
-    meta: Optional[Metadata] = None
     data: Union[List, Dict]
 
-
-class ResponseWarning(Base):
-
-    info: InfoWarning = InfoWarning()
-    meta: Optional[Metadata] = None
-    data: Optional[Union[List, Dict]] = None
-
     @classmethod
-    def set(cls, warnings, data):
-
+    def set(cls, data, metadata=None):
         return cls(
-            info=InfoWarning(warnings=warnings),
+            info=InfoSuccess(metadata=metadata),
             data=data
         )
 
 
-class ResponseError(Base):
+class Warnings(Base):
+
+    info: InfoWarning = InfoWarning()
+    data: Optional[Union[List, Dict]] = None
+
+    @classmethod
+    def set(cls, warnings, data, metadata=None):
+
+        return cls(
+            info=InfoWarning(warnings=warnings, metadata=metadata),
+            data=data
+        )
+
+
+class Errors(Base):
     info: InfoError = InfoError()
 
     @classmethod
-    def set(cls, errors, warnings=[]):
+    def set(cls, errors, warnings=None):
         return cls(
             info=InfoError(
                 warnings=warnings,
                 errors=errors
             )
         )
+
+class Response:
+
+    @classmethod
+    def set(cls, status_code=None, data=None, metadata=None, warnings=None, errors=None):
+
+        status_code = cls.get_status_code(status_code, warnings, errors)
+
+        if errors is not None:
+
+            return JSONResponse(
+                status_code=status_code,
+                content=Errors.set(
+                    errors=errors,
+                    warnings=warnings
+                )
+            )
+        if errors is None and warnings is not None:
+
+            return JSONResponse(
+                status_code=status_code,
+                content=Warnings.set(
+                    warnings=warnings,
+                    data=data,
+                    metadata=metadata
+                )
+            )
+
+        return JSONResponse(
+            status_code=status_code,
+            content=Success.set(
+                data=data,
+                metadata=metadata
+            )
+        )
+
+    @staticmethod
+    def get_status_code(status_code, warnings, errors):
+
+        if isinstance(status_code, int):
+            return status_code
+
+        if errors is not None:
+            return 500
+        if errors is None and warnings is not None:
+            return 299
+
+class Responses:
+
+    @classmethod
+    def get(cls):
+
+        return {
+            200: {"model": Success},
+            299: {"model": Warnings},
+            400: {"model": Errors},
+            422: {"model": Errors},
+            500: {"model": Errors},
+        }
